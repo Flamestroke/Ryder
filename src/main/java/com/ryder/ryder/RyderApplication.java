eaffc51de2d6129d358f08bf8a731ec404484bbd
Flamestroke <guptearchit@gmail.com>
2025-11-17 00:08:34 +0530

    Ryder Project Initialization

---
+++ src/main/java/com/ryder/ryder/RyderApplication.java
@@ -0,0 +1,13 @@
+package com.ryder.ryder;
+
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RyderApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ryder/ryder/RyderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryder.ryder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
